560afaf32ba23b5520ee4c7f3f42f550d1995a52
apteroti <apteroti@gmail.com>
2025-07-18 16:27:15 +0200

    Stop tracking build folder

--- build/CMakeFiles/Archaeotoolbox.dir/Archaeotoolbox_autogen/S5YYZ2ZCNV/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Archaeotoolbox.dir/Archaeotoolbox_autogen/S5YYZ2ZCNV/qrc_resources.cpp.o: \
- /home/kaveh/cpp/ArchaeoToolbox/build/Archaeotoolbox_autogen/S5YYZ2ZCNV/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/documents/readme.txt
+++
@@ -1,4 +0,0 @@
-Linux: qhelpgenerator-qt5 helpdocuments.qhcp -o helpdocuments.qhc
-
-Windows: .\qhelpgenerator.exe C:\cpp\ToolBox\build\documents\helpdocuments.qhcp -o C:\cpp\ToolBox\build\documents\helpdocuments.qhc
-
